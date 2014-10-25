23b13f28179a259ede86931bce5753e96b6331c2
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-10-25 11:53:51 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 'use strict';
 
-var fs      = require('fs'),
-    path    = require("path"),
+var config  = require(__dirname + '/config/config'),
 
-    config  = require('./config'),
+
 
     restify = require('restify'),
     server  = restify.createServer({
       name: config.server.name
     }),
 
-    routes = require('./routes')(server),
+    db     = require(config.root + '/config/db'),
 
-    Sequelize = require('sequelize'),
-    sequelize = new Sequelize(config.db.name, config.db.username, config.db.password, {
-      dialect: config.db.dialect,
-      port: config.db.port,
-    }),
-    db = {};
+    routes = require(config.root + '/routes')(server);
 
-fs.readdirSync(config.models.path)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0);
-  }).forEach(function(file) {
-    var model = sequelize["import"](path.join(config.models.path, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
-
-sequelize.sync().success(function() {
+db.sequelize.sync().success(function() {
   server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
---
+++ config/db.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var fs        = require('fs'),
+    path      = require('path'),
+    Sequelize = require('sequelize'),
+    config    = require(__dirname + '/../config/config'),
+    sequelize = new Sequelize(config.db.name, config.db.username, config.db.password, {
+      dialect: config.db.dialect,
+      port: config.db.port,
+    }),
+    db        = {};
+
+fs.readdirSync(config.models.path)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== "index.js");
+  })
+  .forEach(function(file) {
+    var model = sequelize['import'](path.join(config.models.path, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
