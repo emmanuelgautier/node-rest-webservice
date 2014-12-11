733db1cb12dd51aa9f2dc692976d22b042baf6e9
Emmanuel Gautier <manu.gautier1394@gmail.com>
2014-12-11 12:19:31 +0100

    fix typo

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@
 
 /*jshint maxlen: 1000 */
 
-var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+var path      = require('path'),
+    rootPath  = path.normalize(__dirname + '/..'),
+    env       = process.env.NODE_ENV || 'development';
 
 var config = {
   env: env,
