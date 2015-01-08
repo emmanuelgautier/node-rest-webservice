cea97e14dd738bcdcf5bf4114cfc0614c19fe6b7
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-01-08 15:26:20 +0100

    add session support

--- config/server.js
+++ config/server.js
@@ -1,9 +1,25 @@
 'use strict';
 
-var restify = require('restify');
+var restify = require('restify'),
+
+  sessions = require('client-sessions');
 
 module.exports = function(server) {
+
   server.use(restify.acceptParser(server.acceptable));
-  server.use(restify.bodyParser({ mapParams: false }));
-  server.use(restify.queryParser({ mapParams: false }));
+
+  server.use(restify.bodyParser({
+    mapParams: false
+  }));
+
+  server.use(restify.queryParser({
+    mapParams: false
+  }));
+
+  server.use(sessions({
+    cookieName: 'sid',
+    secret: 'key',
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 1000 * 60 * 5
+  }));
 };
