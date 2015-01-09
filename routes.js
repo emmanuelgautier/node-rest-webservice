972a9e8d497f248fb5e12eac3368b877b883b5f0
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-01-09 10:03:11 +0100

    cleaning routes

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var config    = require(__dirname + '/config/config'),
+var passport  = require('passport'),
 
-    passport  = require('passport'),
+    config    = require(__dirname + '/config/config'),
 
     authors   = require(config.controllers.path + '/authors'),
     books     = require(config.controllers.path + '/books'),
@@ -12,26 +12,26 @@ var config    = require(__dirname + '/config/config'),
 
 module.exports = function(server) {
 
-  server.get('/authors', authors.list);
-  server.post('/authors', authors.create);
-  server.get('/authors/:id', authors.get);
-  server.put('/authors/:id', authors.update);
-  server.del('/authors/:id', authors.delete);
+  server.get('/authors',            authors.list);
+  server.post('/authors',           authors.create);
+  server.get('/authors/:id',        authors.get);
+  server.put('/authors/:id',        authors.update);
+  server.del('/authors/:id',        authors.delete);
 
-  server.get('/authors/:id/books', books.listByAuthor);
-  server.get('/editors/:id/books', books.listByEditor);
+  server.get('/authors/:id/books',  books.listByAuthor);
+  server.get('/editors/:id/books',  books.listByEditor);
 
-  server.get('/books', books.list);
-  server.post('/books', books.create);
-  server.get('/books/:id', books.get);
-  server.put('/books/:id', books.update);
-  server.del('/books/:id', books.delete);
+  server.get('/books',              books.list);
+  server.post('/books',             books.create);
+  server.get('/books/:id',          books.get);
+  server.put('/books/:id',          books.update);
+  server.del('/books/:id',          books.delete);
 
-  server.get('/editors', editors.list);
-  server.post('/editors', editors.create);
-  server.get('/editors/:id', editors.get);
-  server.put('/editors/:id', editors.update);
-  server.del('/editors/:id', editors.delete);
+  server.get('/editors',            editors.list);
+  server.post('/editors',           editors.create);
+  server.get('/editors/:id',        editors.get);
+  server.put('/editors/:id',        editors.update);
+  server.del('/editors/:id',        editors.delete);
 
   server.get('/dialog/authorize', oauth2.authorize);
   server.post('/dialog/authorize/decision', oauth2.decision);
