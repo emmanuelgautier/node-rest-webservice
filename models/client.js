c4490443303ecce2d68702a6a4ca97fcb30eb41e
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-01-09 10:05:24 +0100

    cleaning models

--- models/client.js
+++ models/client.js
@@ -2,9 +2,9 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Client = sequelize.define('Client', {
-    name: { type: DataTypes.STRING,  allowNull: false },
-    id: { type: DataTypes.INTEGER,  allowNull: false },
-    secret: { type: DataTypes.STRING,  allowNull: false },
+    name:   { type: DataTypes.STRING,   allowNull: false },
+    id:     { type: DataTypes.INTEGER,  allowNull: false },
+    secret: { type: DataTypes.STRING,   allowNull: false },
     userId: { type: DataTypes.INTEGER,  allowNull: false }
   }, {
     tableName: 'client',
--- models/code.js
+++ models/code.js
@@ -2,10 +2,10 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Code = sequelize.define('Code', {
-    value: { type: DataTypes.STRING, allowNull: false },
-    redirectUri: { type: DataTypes.STRING, allowNull: false },
-    userId: { type: DataTypes.INTEGER, allowNull: false },
-    clientId: { type: DataTypes.INTEGER, allowNull: false }
+    value:        { type: DataTypes.STRING,   allowNull: false },
+    redirectUri:  { type: DataTypes.STRING,   allowNull: false },
+    userId:       { type: DataTypes.INTEGER,  allowNull: false },
+    clientId:     { type: DataTypes.INTEGER,  allowNull: false }
   }, {
     tableName: 'code',
     timestamps: false,
