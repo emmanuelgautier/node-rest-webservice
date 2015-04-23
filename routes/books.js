ebef62459702e1b89466f2713e777c2c03da1c2e
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-04-23 11:59:10 +0200

    fix routes indent

--- routes/books.js
+++ routes/books.js
@@ -4,33 +4,34 @@ var config = require('../config/config'),
     books = require(config.paths.controllers + '/books');
 
 var routes = [{
-  method: 'GET',
-  path: '/books/',
-  handler: books.list
-}, {
-  method: 'POST',
-  path: '/books/',
-  handler: books.create
-}, {
-  method: 'GET',
-  path: '/books/{book}',
-  handler: books.get
-}, {
-  method: 'PUT',
-  path: '/books/{book}',
-  handler: books.update
-}, {
-  method: 'DELETE',
-  path: '/books/{book}',
-  handler: books.delete
-}, {
-  method: 'GET',
-  path: '/authors/{author}/books',
-  handler: books.listByAuthor
-}, {
-  method: 'GET',
-  path: '/editors/{editor}/books',
-  handler: books.listByEditor
-}];
+    method: 'GET',
+    path: '/books/',
+    handler: books.list
+  }, {
+    method: 'POST',
+    path: '/books/',
+    handler: books.create
+  }, {
+    method: 'GET',
+    path: '/books/{book}',
+    handler: books.get
+  }, {
+    method: 'PUT',
+    path: '/books/{book}',
+    handler: books.update
+  }, {
+    method: 'DELETE',
+    path: '/books/{book}',
+    handler: books.delete
+  }, {
+    method: 'GET',
+    path: '/authors/{author}/books',
+    handler: books.listByAuthor
+  }, {
+    method: 'GET',
+    path: '/editors/{editor}/books',
+    handler: books.listByEditor
+  }
+];
 
 module.exports = routes;
--- routes/editors.js
+++ routes/editors.js
@@ -4,25 +4,26 @@ var config = require('../config/config'),
     editors = require(config.paths.controllers + '/editors');
 
 var routes = [{
-  method: 'GET',
-  path: '/editors/',
-  handler: editors.list
-}, {
-  method: 'POST',
-  path: '/editors/',
-  handler: editors.create
-}, {
-  method: 'GET',
-  path: '/editors/{editor}',
-  handler: editors.get
-}, {
-  method: 'PUT',
-  path: '/editors/{editor}',
-  handler: editors.update
-}, {
-  method: 'DELETE',
-  path: '/editors/{editor}',
-  handler: editors.delete
-}];
+    method: 'GET',
+    path: '/editors/',
+    handler: editors.list
+  }, {
+    method: 'POST',
+    path: '/editors/',
+    handler: editors.create
+  }, {
+    method: 'GET',
+    path: '/editors/{editor}',
+    handler: editors.get
+  }, {
+    method: 'PUT',
+    path: '/editors/{editor}',
+    handler: editors.update
+  }, {
+    method: 'DELETE',
+    path: '/editors/{editor}',
+    handler: editors.delete
+  }
+];
 
 module.exports = routes;
