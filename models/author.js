f43515076f70762b1c7bf1fb2675f0898ebb0efc
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-04-24 10:46:09 +0200

    more correct gender datatype

--- models/author.js
+++ models/author.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     gender: {
-      type: DataTypes.BOOLEAN,
+      type: DataTypes.ENUM('1', '2'),
       allowNull: false
     },
     birth_date: {
